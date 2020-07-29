16d99334306992f4efb8e8f7d547683ee2e189a3
akane0303 <skycloud338a@gmail.com>
2020-07-29 20:50:40 +0900

    commit

--- include/robo/imu_filter.h
+++ include/robo/imu_filter.h
@@ -18,11 +18,7 @@ class ImuFilter{
             zeta=zeta_;
         }
         void getorientation(double& q0,double& q1,double& q2,double& q3){
-            q0=this->q0;
-            q1=this->q1;
-            q2=this->q2;
-            q3=this->q3;
         }
 
     
-}
+};
